f458f992190576095f411dd03d51885a89ec4f45
Lewis Ryan <wiesl.yarn@gmail.com>
2023-06-14 10:04:50 -0700

    Update GameOver.js added play again instruction

--- tutorials/bunny-jump/src/scenes/GameOver.js
+++ tutorials/bunny-jump/src/scenes/GameOver.js
@@ -13,6 +13,10 @@ export default class GameOver extends Phaser.Scene {
         this.add.text(width * 0.5, height * 0.5, 'Game Over', {
             fontSize: 48
         }).setOrigin(0.5);
+        
+        this.add.text(width * 0.5, height * 0.75, 'press spacebar to play again', {
+            fontSize: 28
+        }).setOrigin(0.5);
 
         this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.start('game');
